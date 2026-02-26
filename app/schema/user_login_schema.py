93f6b0aad4ecceeac6fd154dc98eb73ce2a426d9
devanshlamaniya-gamma <devansh.lamaniya@gammaedge.io>
2026-02-27 01:27:33 +0530

    deploying to railway

--- app/schema/user_login_schema.py
+++ app/schema/user_login_schema.py
@@ -2,8 +2,6 @@ from pydantic import BaseModel, EmailStr
 
 
 class UserLogin(BaseModel):
+
     email: EmailStr
     password: str
-
-
-#the pydantic is the module and the basemodel is the class  that checks the input values that they are correct or not this ensure that the input will be in correct form
